056331b87af034b31b033fb2033e01dffc8d2254
Akanksha Memane <memaneakanksha30@gmail.com>
2026-04-03 21:26:27 +0530

    added get in touch and yesyimonial page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,11 +1,15 @@
 import HeroBanner from "../components/Banner";
 import OurProcess from "../components/OurProcess";
+import Reviews from "../components/Reviews";
+import GetInTouch from "../components/GetInTouch";
 
 export default function Home() {
   return (
     <>
       <HeroBanner />
       <OurProcess />
+      <Reviews />
+      <GetInTouch />
     </>
   );
 }
