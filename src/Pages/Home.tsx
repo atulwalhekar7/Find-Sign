1baaa0d0da6e6d7b771bbc034270522104592570
tarunachoudhary43-hash <tarunachoudhary43@gmail.com>
2026-04-04 18:30:04 -0400

    .

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,11 +1,13 @@
 import HeroBanner from "../components/Banner";
 import OurProcess from "../components/OurProcess";
 
+
 export default function Home() {
   return (
     <>
       <HeroBanner />
       <OurProcess />
+      
     </>
   );
 }
