5a08ddd22727fa6d7450f4805896c82f96b34718
tarunachoudhary43-hash <tarunachoudhary43@gmail.com>
2026-04-24 22:41:44 -0400

    Change the About page  about section image

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -6,7 +6,7 @@ import SimpleGetInTouch from "../components/GetInTouch";
 import SimpleFooter from "../components/SimpleFooter";
 import OurProcess from "../components/OurProcess";
 import Banner from "../assets/About Us_Banner.jpg";
-import AboutUsBanner from "../assets/About Find&Sign.jpg";
+import AboutUsBanner from "../assets/About Find & Sign 2.jpg";
 import Bec from "../assets/Bec_Bio.jpg";
 import niki from "../assets/Niki_Bio.jpg";
 import Image1 from "../components/Image1";
