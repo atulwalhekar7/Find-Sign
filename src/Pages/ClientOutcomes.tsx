112f79b894216500c88759b7bf505a034efe0355
tarunachoudhary43-hash <tarunachoudhary43@gmail.com>
2026-04-24 22:36:41 -0400

    In Client Outcomes change About Find & Sign text to About Client Outcomes

--- src/Pages/ClientOutcomes.tsx
+++ src/Pages/ClientOutcomes.tsx
@@ -918,7 +918,7 @@ export default function ClientOutcomes() {
       {/* ── ABOUT ───────────────────────────────── */}
       <AboutSection
         imageSrc={AboutClientOutcomesImg}
-        heading="About Find & Sign"
+        heading="About Client Outcomes"
         subheading=""
         body1="Body text for your whole article or post. We'll put in some lorem ipsum to show how a filled-out page might look."
         body2="Excepteur efficient emerging, minim veniam anim aute carefully curated Ginza conversation exquisite perfect nostrud nisi intricate Content. Qui international first-class nulla ut. Punctual adipisicing, essential lovely queen tempor eiusmod irure. Exclusive izakaya charming Scandinavian impeccable aute quality of life soft power pariatur Melbourne occaecat discerning. Qui wardrobe aliquip, et Porter destination Toto remarkable officia Helsinki excepteur Basset hound. Zürich sleepy perfect consectetur."
