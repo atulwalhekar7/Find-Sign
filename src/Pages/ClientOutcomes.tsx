1ad16c21b786b0ab1834718e5b975ae9c1f36a86
tarunachoudhary43-hash <tarunachoudhary43@gmail.com>
2026-05-18 12:05:27 -0400

    Made the image height and para height same

--- src/Pages/ClientOutcomes.tsx
+++ src/Pages/ClientOutcomes.tsx
@@ -7,7 +7,7 @@ import Image5 from "../components/Image5";
 import Image6 from "../components/Image6";
 
 import bannerImg from "../assets/happy-home-buyers-australia-Clientoutcomes-banner-find-and-sign.jpg";
-import AboutClientOutcomesImg from "../assets/About Find&Sign.jpg";
+import AboutClientOutcomesImg from "../assets/About Find&Sign-clientoutcomes-aboutsection-image.jpg";
 
 import id1 from "../assets/Client Outcomes/id1.webp";
 import id2 from "../assets/Client Outcomes/id2.webp";
